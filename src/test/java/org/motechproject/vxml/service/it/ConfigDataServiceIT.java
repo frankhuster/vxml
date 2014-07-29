37c8d38b3927b31e528ccbc1e6e6ccaa69d036f2
frankie <fhuster@grameenfoundstion.org>
2014-07-29 09:22:39 -0700

    some progress: it's a classloader issue

--- src/test/java/org/motechproject/vxml/service/it/ConfigDataServiceIT.java
+++ src/test/java/org/motechproject/vxml/service/it/ConfigDataServiceIT.java
@@ -18,9 +18,11 @@ import org.slf4j.LoggerFactory;
 import javax.inject.Inject;
 import javax.jdo.JDOException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Verify ConfigDataService present & functional.
@@ -31,6 +33,40 @@ import static org.junit.Assert.assertEquals;
 public class ConfigDataServiceIT extends BasePaxIT {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private boolean areMapsEqual(Map<String, CallStatus> m1, Map<String, CallStatus> m2) {
+        if (m1 == m2) {
+            return true;
+        }
+
+        if (m1.size() != m2.size()) {
+            return false;
+        }
+
+        try {
+            Iterator<Map.Entry<String,CallStatus>> i = m1.entrySet().iterator();
+            while (i.hasNext()) {
+                Map.Entry<String,CallStatus> e = i.next();
+                String key = e.getKey();
+                CallStatus value = CallStatus.FAILED;
+                // this throws a ClassCastException - grrr...
+                value = e.getValue();
+                if (value == null) {
+                    if (!(m2.get(key)==null && m2.containsKey(key)))
+                        return false;
+                } else {
+                    if (!value.equals(m2.get(key)))
+                        return false;
+                }
+            }
+        } catch (ClassCastException unused) {
+            return false;
+        } catch (NullPointerException unused) {
+            return false;
+        }
+
+        return true;
+    }
 
     @Inject
     private ConfigDataService configDataService;
@@ -44,18 +80,24 @@ public class ConfigDataServiceIT extends BasePaxIT {
     public void verifyServiceFunctional() throws Exception {
         logger.info("verifyServiceFunctional");
 
-        Map<String, CallStatus> statusMap = new HashMap<>();
-        statusMap.put("answered", CallStatus.ANSWERED);
+        ClassLoader old = Thread.currentThread().getContextClassLoader();
+        try {
+            Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
+            Map<String, CallStatus> statusMap = new HashMap<>();
+            statusMap.put("answered", CallStatus.ANSWERED);
 
-        Map<String, String> callDetailMap = new HashMap<>();
-        callDetailMap.put("recipient", "to");
+            Map<String, String> callDetailMap = new HashMap<>();
+            callDetailMap.put("recipient", "to");
 
-        configDataService.create(new Config("MyConfig", statusMap, callDetailMap));
+            configDataService.create(new Config("MyConfig", statusMap, callDetailMap));
 
-        Config config = configDataService.findByName("MyConfig");
-        assertEquals(config.name, "MyConfig");
-        assertEquals(config.callDetailMap, callDetailMap);
-        // Why oh why does this not work?!?
-        assertEquals(config.statusMap, statusMap);
+            Config config = configDataService.findByName("MyConfig");
+            assertEquals(config.name, "MyConfig");
+            assertEquals(config.callDetailMap, callDetailMap);
+            // Why oh why does this not work?!? See inside areMapsEqual() above for more details
+            assertTrue(areMapsEqual(config.statusMap, statusMap));
+        } finally {
+            Thread.currentThread().setContextClassLoader(old);
+        }
     }
 }
